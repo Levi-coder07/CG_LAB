c6715ef16e3b3f5472aeadd87b877ecbb4944e7a
Levi Castillon <82822546+Levi-coder07@users.noreply.github.com>
2023-06-12 11:26:32 -0500

    Update main2.cpp

--- src/main2.cpp
+++ src/main2.cpp
@@ -9,7 +9,7 @@
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtc/type_ptr.hpp>
-
+#include <vector>
 
 // Vertices coordinates
 /*GLfloat vertices[] =
@@ -36,7 +36,7 @@ GLfloat vertices[] = {
     0.25f, -0.05f * float(sqrt(3)), 0.0f,      0.9f, 0.2f, 0.2f, // Bottom right
     0.0f, 0.25f * float(sqrt(3)), 0.0f,        0.9f, 0.2f, 0.2f, // Top
 
-    // Base
+   
     -0.25f, -0.05f * float(sqrt(3)), 0.0f,     0.2f, 0.8f, 0.2f, // Bottom left
     0.25f, -0.05f * float(sqrt(3)), 0.0f,      0.2f, 0.8f, 0.2f, // Bottom right
     -0.25f, -0.2f * float(sqrt(3)), 0.0f,      0.2f, 0.8f, 0.2f, // Top left
@@ -45,7 +45,6 @@ GLfloat vertices[] = {
     0.25f, -0.05f * float(sqrt(3)), 0.0f,      0.2f, 0.8f, 0.2f, // Bottom right
     0.25f, -0.2f * float(sqrt(3)), 0.0f,       0.2f, 0.8f, 0.2f, // Top right
 
-    // Door
     -0.075f, -0.2f * float(sqrt(3)), 0.0f,    0.5f, 0.5f, 0.8f, // Bottom left
     0.075f, -0.2f * float(sqrt(3)), 0.0f,     0.5f, 0.5f, 0.8f, // Bottom right
     -0.075f, -0.125f * float(sqrt(3)), 0.0f,  0.5f, 0.5f, 0.8f, // Top left
@@ -53,19 +52,46 @@ GLfloat vertices[] = {
     -0.075f, -0.125f * float(sqrt(3)), 0.0f,  0.5f, 0.5f, 0.8f, // Top left
     0.075f, -0.2f * float(sqrt(3)), 0.0f,     0.5f, 0.5f, 0.8f, // Bottom right
     0.075f, -0.125f * float(sqrt(3)), 0.0f,   0.5f, 0.5f, 0.8f,
-	 -0.15f, 0.0f, 0.0f,                        0.4f, 0.4f, 0.4f, // Abstract point 1
-    0.15f, 0.0f, 0.0f,                         0.4f, 0.4f, 0.4f, // Abstract point 2
+	 -0.15f, 0.0f, 0.0f,                        0.4f, 0.4f, 0.4f, 
+    0.15f, 0.0f, 0.0f,                         0.4f, 0.4f, 0.4f, 
     0.0f, 0.2f * float(sqrt(3)), 0.0f,          0.4f, 0.4f, 0.4f,
 	  0.6f, -0.2f * float(sqrt(3)), 0.0f,       0.36f, 0.25f, 0.2f, // Bottom right
   0.8f, -0.2f * float(sqrt(3)), 0.0f,       0.36f, 0.25f, 0.2f, // Bottom left
   0.7f, 0.0f * float(sqrt(3)), 0.0f,        0.36f, 0.25f, 0.2f, // Top
-
-  // Leaves
-  0.6f, 0.0f * float(sqrt(3)), 0.0f,       0.4f, 0.4f, 0.0f, // Bottom right
+0.6f, 0.0f * float(sqrt(3)), 0.0f,       0.4f, 0.4f, 0.0f, // Bottom right
   0.8f, 0.0f * float(sqrt(3)), 0.0f,       0.4f, 0.4f, 0.0f, // Bottom left
   0.7f, 0.15f * float(sqrt(3)), 0.0f,        0.4f, 0.4f, 0.0f, // Top
+  
 
-    // Leaves - Bottom left // Abstract point 3 // Top right
+        -0.8f, 0.6f, 0.0f,                       0.9f, 0.2f, 0.2f, // Bottom left
+        -0.6f, 0.6f, 0.0f,                       0.9f, 0.2f, 0.2f, // Bottom right
+        -0.7f, 0.8f, 0.0f,                       0.9f, 0.2f, 0.2f, // Top
+
+    
+        -0.8f, 0.6f, 0.0f,                       0.9f, 0.2f, 0.2f, // Bottom left
+        -0.7f, 0.4f, 0.0f,                       0.9f, 0.2f, 0.2f, // Top left
+        -0.7f, 0.8f, 0.0f,                       0.9f, 0.2f, 0.2f, // Center
+
+     
+        -0.6f, 0.6f, 0.0f,                       0.9f, 0.2f, 0.2f, // Bottom right
+        -0.7f, 0.4f, 0.0f,                       0.9f, 0.2f, 0.2f, // Top right
+        -0.7f, 0.8f, 0.0f,                       0.9f, 0.2f, 0.2f, // Center
+
+       
+        -0.7f, 0.4f, 0.0f,                       0.2f, 0.8f, 0.2f, // Bottom left
+        -0.75f, 0.35f, 0.0f,                     0.2f, 0.8f, 0.2f, // Top left
+        -0.65f, 0.35f, 0.0f,                     0.2f, 0.8f, 0.2f, 
+
+        
+        -0.7f, 0.35f, 0.0f,                      0.5f, 0.5f, 0.8f, 
+        -0.75f, 0.35f, 0.0f,                     0.5f, 0.5f, 0.8f, 
+        -0.7f, 0.3f, 0.0f,                       0.5f, 0.5f, 0.8f, 
+
+        -0.7f, 0.35f, 0.0f,                      0.5f, 0.5f, 0.8f, 
+        -0.65f, 0.35f, 0.0f,                     0.5f, 0.5f, 0.8f, 
+        -0.7f, 0.3f, 0.0f,                       0.5f, 0.5f, 0.8f,
+        
+    
 };
 // Indices for vertices order
 GLuint indices[] = {
@@ -81,7 +107,13 @@ GLuint indices[] = {
     12, 13, 14,
 	15,16,17,
 	18,19,20,
-	21,22,23
+	21,22,23,
+    24,25,26,
+    27,28,29,
+    30,31,32,
+    33,34,35,
+    36,37,38,
+    39,40,41
 };
 
 /*GLuint indices[] =
@@ -89,9 +121,60 @@ GLuint indices[] = {
 	6,3,0,
 	4,5,1
 };*/
+/*GLuint vbo;
+GLuint vao;
+const int CIRCLE_SEGMENTS = 100;
+const float PI = 3.14159265359f;
+void initializeCircle(float centerX, float centerY, float radius)
+{
+    std::vector<float> vertices2;
+    float posX = -0.9f;
+    float posY = 0.9f;
+    // Calculate the vertices of the circle
+    for (int i = 0; i < CIRCLE_SEGMENTS; ++i) {
+        float theta = 2.0f * PI * float(i) / float(CIRCLE_SEGMENTS);
+        float x = radius * std::cos(theta) + posX;
+        float y = radius * std::sin(theta) + posY;
 
+        vertices2.push_back(centerX + x);
+        vertices2.push_back(centerY + y);
+    }
 
+    // Create and bind a vertex buffer object (VBO)
+    glGenBuffers(1, &vbo);
+    glBindBuffer(GL_ARRAY_BUFFER, vbo);
 
+    // Upload vertex data to the VBO
+    glBufferData(GL_ARRAY_BUFFER, vertices2.size() * sizeof(float), vertices2.data(), GL_STATIC_DRAW);
+
+    // Create and bind a vertex array object (VAO)
+    glGenVertexArrays(1, &vao);
+    glBindVertexArray(vao);
+
+    // Enable and configure the vertex attribute array
+    glEnableVertexAttribArray(0);
+    glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 0, 0);
+
+    // Unbind the VBO and VAO
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+    glBindVertexArray(0);
+}
+
+// Render the circle
+void renderCircle()
+{
+    // Bind the VBO and VAO
+    glBindBuffer(GL_ARRAY_BUFFER, vbo);
+    glBindVertexArray(vao);
+
+    // Render the circle
+    glDrawArrays(GL_LINE_LOOP, 0, CIRCLE_SEGMENTS);
+
+    // Unbind the VBO and VAO
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+    glBindVertexArray(0);
+}
+*/
 int main()
 {
 	// Initialize GLFW
@@ -156,64 +239,77 @@ int main()
 
 	// Gets ID of uniform called "scale"
 	GLuint uniID = glGetUniformLocation(shaderProgram.ID, "scale");
-
-	
+   
+	 bool reverseDirection = false;
+     float translationSpeed = 0.3f;
+    const float translationLimit = 0.2f;
 	// Main while loop
+    float previousTime = glfwGetTime();
+    float translationX = 0.0f;
+    //initializeCircle(0.0f, 0.0f, 0.2f);
+    float rotationAngle = 0.0f;
 	while (!glfwWindowShouldClose(window))
 {
     // Specify the color of the background
     glClearColor(0.07f, 0.13f, 0.17f, 1.0f);
     // Clean the back buffer and assign the new color to it
     glClear(GL_COLOR_BUFFER_BIT);
+     
+
 
     float time = glfwGetTime();
     float scaleFactor1 = 0.35f + sin(time) * 0.3f; // Scale factor for the first triangle
     float scaleFactor2 = 0.65f + sin(time) * 0.7f; // Scale factor for the second triangle
     glm::vec3 changingColor(scaleFactor1, scaleFactor2 , scaleFactor2);
 
-    // Get the location of the "baseColor" uniform
     GLint baseColorLoc = glGetUniformLocation(shaderProgram.ID, "baseColor");
-    // Set the value of the "baseColor" uniform
+
     glUniform3fv(baseColorLoc, 1, glm::value_ptr(changingColor));
     // Tell OpenGL which Shader Program we want to use
     shaderProgram.Activate();
-    VAO1.Bind();
-
-    glm::mat4 transform = glm::scale(glm::mat4(1.0f), glm::vec3(scaleFactor1));
-	transform = glm::translate(transform, glm::vec3(-0.8f, -0.8f, 0.0f));
-	transform = glm::rotate(transform, (float)glfwGetTime(), glm::vec3(0.0f, 0.0f, 1.0f));
-    //glm::mat4 transform2 = glm::scale(glm::mat4(1.0f), glm::vec3(scaleFactor2));
-    // Assign transformation matrix to the uniform
-	  float objectWidth = 10;
-    float objectHeight = 10;
-    float viewportWidth = 100;
-    float viewportHeight = 100;
-
-    // Check if the object exceeds the viewport edges
-    if (transform[3].x + objectWidth / 2 > viewportWidth / 2)
-        transform[3].x = viewportWidth / 2 - objectWidth / 2;
-    else if (transform[3].x - objectWidth / 2 < -viewportWidth / 2)
-        transform[3].x = -viewportWidth / 2 + objectWidth / 2;
-
-    if (transform[3].y + objectHeight / 2 > viewportHeight / 2)
-        transform[3].y = viewportHeight / 2 - objectHeight / 2;
-    else if (transform[3].y - objectHeight / 2 < -viewportHeight / 2)
-        transform[3].y = -viewportHeight / 2 + objectHeight / 2;
-    glUniformMatrix4fv(uniID, 1, GL_FALSE, glm::value_ptr(transform));
-	/*int viewportWidth = 800;  // Set your desired width
-    int viewportHeight = 600; // Set your desired height
-    int viewportX = (1000- viewportWidth) / 2;   // Center the viewport horizontally
-    int viewportY = (1000 - viewportHeight) / 2; // Center the viewport vertically
-    glViewport(viewportX, viewportY, viewportWidth, viewportHeight);*/
     // Bind the VAO so OpenGL knows to use it
+    VAO1.Bind();
+    float time3 = glfwGetTime() * 45 ;
+    float currentTime = glfwGetTime();
+    float deltaTime = currentTime - previousTime;
+    previousTime = currentTime;
+   
+    glm::mat4 transform = glm::scale(glm::mat4(1.0f), glm::vec3(scaleFactor1));
+	//glm::mat4 transform2 = glm::translate(glm::mat4(1.0f), glm::vec3(-0.0f, -0.0f, 0.0f));
+	
+    //glm::mat4 transform2 = glm::scale(glm::mat4(1.0f), glm::vec3(scaleFactor2));
+ 
+    translationX += deltaTime * translationSpeed;
+    std::cout<< deltaTime <<translationX << std::endl;
+    if (translationX >= translationLimit || translationX <= -translationLimit -0.2) {
+   
+        translationSpeed = -translationSpeed;
+    }
 
+    
+    if (reverseDirection)
+        translationX = -translationX;
+
+    glm::mat4 transform2 = glm::translate(glm::mat4(1.0f), glm::vec3(translationX, 0.0f, 0.0f));
+
+    glUniformMatrix4fv(uniID, 1, GL_FALSE, glm::value_ptr(transform));
+
+  
+    
     // Draw primitives, number of triangles, datatype of indices, index of indices
-    glDrawElements(GL_TRIANGLES, 24, GL_UNSIGNED_INT, (void*)0);
+    glDrawElements(GL_TRIANGLES, 18, GL_UNSIGNED_INT, (void*)0);
+    glUniformMatrix4fv(uniID, 1, GL_FALSE, glm::value_ptr(transform2));
     //glUniformMatrix4fv(uniID, 1, GL_FALSE, glm::value_ptr(transform2));
-    //glDrawElements(GL_TRIANGLES, 3, GL_UNSIGNED_INT, (void*)(3 * sizeof(float)));
+    glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, (void*)(18 * sizeof(float)));
     //glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
-
+    glm::mat4 transform3 = glm::mat4(1.0f);
+    transform3 = glm::translate(transform3, -(glm::vec3(+0.6f, -0.6f,0.0f)));  
+    transform3 = glm::rotate(transform3, glm::radians(time3), glm::vec3( 0.0f,0.0f,1.0f));
+    transform3 = glm::translate(transform3, (glm::vec3(+0.6f, -0.6f, 0.0f))); 
+    glUniformMatrix4fv(uniID, 1, GL_FALSE, glm::value_ptr(transform3));
+    glDrawElements(GL_TRIANGLES, 18, GL_UNSIGNED_INT, (void*)(24 * sizeof(float)));
     // Swap the back buffer with the front buffer
+    //renderCircle();
     glfwSwapBuffers(window);
     // Take care of all GLFW events
     glfwPollEvents();
